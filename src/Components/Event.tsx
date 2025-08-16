443cc191359ce703b18fde369b1c6b5b57b92b9c
Liammmbr <bradyliam00@gmail.com>
2025-08-16 11:36:10 -0400

    Add basic event functionality and button to create events

---
+++ src/Components/Event.tsx
@@ -0,0 +1,13 @@
+function Event() {
+  return (
+    <div>
+      <form>
+        <input name="title" type="text" placeholder={"Title"} />
+        <input name="date" type="date" />
+        <input name="time" type="time" />
+      </form>
+    </div>
+  );
+}
+
+export default Event;
